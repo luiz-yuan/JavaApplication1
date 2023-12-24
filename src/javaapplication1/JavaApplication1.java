a6331f6483aec2b200460878f0c5f00db97fa32b
luiz-yuan <luizyuan@gmail.com>
2023-12-24 08:10:39 -0300

    primeiro commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+/**
+ *
+ * @author luiz
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste!!!");
+    }
+    
+}
